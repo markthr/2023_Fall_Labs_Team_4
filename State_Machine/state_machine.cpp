c5b8ba7836ed1e504098dc63a9108623d7fb1939
Mark <markbthr@gmail.com>
2023-10-19 01:51:05 -0400

    Abstract State definition
    Move definition for enums and macro into header
    
    Defined abstract State class for defining structure of what a State is. For this, used the entry, do, exit model for breaking down a state's behavior.
    
    Using the model where a state's execution should be "brief" so that if it wishes to loop, it should return "Loop" instead of just running forever. This allows common functionality across states like reading in sensor values to be outside of a step. Sensor values are (optionally) read into a buffer before a state executes and then the buffer is passed by reference to the state.

--- State_Machine/state_machine.cpp
+++ State_Machine/state_machine.cpp
@@ -1,23 +1,7 @@
 #include <array>
 #include <iostream>
-
-#define NAME(Variable) (#Variable)
-
-/**
- * @brief 
- * Startup: the system has just been turned on
- * Forward: moving over the line
- * Gap: the line has ended, continue on for a bit to see if it is a dashed line
- * Detection: the object of interest has been detected at the end of a branching path
- * Stop: finished navigating
- */
-enum class State {Startup, Forward, Branch, Backtrack, Gap, Detection, Stop, _Count};
-
-enum class Branch_Direction {Right, Left};
-
-
-
-enum class Return_Value {Loop, Timeout, Intersection, Line_Loss, Complete, _Count};
+#include<string>
+#include "state.h"
 
 /*
  * Don't overengineer. No need to program for the most general case. Just the case presented in class.
@@ -29,50 +13,88 @@ enum class Return_Value {Loop, Timeout, Intersection, Line_Loss, Complete, _Coun
  * 
  */
 // use std::array for safety
-// inner array indexes the return values for a given state. 
-// outer array indexes the states
-std::array<std::array<State, (int)Return_Value::_Count>, (int)State::_Count> transitions;
+// inner array indexes the return values for a given State_Code. 
+// outer array indexes the States
+std::array<std::array<State_Code, (int)Return_Value::_Count>, (int)State_Code::_Count> transitions;
+
+// placeholder for actual input and output
+struct Output {
+    std::string str;
+};
+
+struct Input {
+    std::string str;
+};
+
+class State: public Abstract_State<Input, Output> {
+    public:
+        using Abstract_State<Input, Output>::Abstract_State;
+        Return_Value guard(Input &input) override {
+            std::cout << "Input: " << input.str << std::endl;
+            return Return_Value::Complete;
+        }
+
+        void do_behavior(Input &input, Output &output) override {
+            std::cout << "Old Output: " << output.str << std::endl;
+            output.str = "The new one";
+        }
+};
+
+State state = State(State_Code::Startup);
 
 void setup() {
     // make it so garbage stops the bot
-    for(int i=0; i<(int)State::_Count; i++) {
+    for(int i=0; i<(int)State_Code::_Count; i++) {
         for(int j=0; j<(int)Return_Value::_Count; j++) {
-            transitions[i][j] = State::Stop;
+            transitions[i][j] = State_Code::Stop;
         }
     }
     
 
-    // make it so loop has the state machine stay in the same state
-    for(int i=0; i<(int)State::_Count; i++) {
-        transitions[i][(int)Return_Value::Loop] = static_cast<State>((size_t)i+1);
+    // make it so loop has the State_Code machine stay in the same State_Code
+    for(int i=0; i<(int)State_Code::_Count; i++) {
+        transitions[i][(int)Return_Value::Loop] = static_cast<State_Code>((size_t)i+1);
     }
     
     // Startup Transitions
-    transitions[(int)State::Startup][(int)Return_Value::Timeout] = State::Forward;
+    transitions[(int)State_Code::Startup][(int)Return_Value::Timeout] = State_Code::Forward;
 
     // Forward Transitions
-    transitions[(int)State::Forward][(int)Return_Value::Line_Loss] = State::Gap;
-    transitions[(int)State::Forward][(int)Return_Value::Intersection] = State::Branch;
+    transitions[(int)State_Code::Forward][(int)Return_Value::Line_Loss] = State_Code::Gap;
+    transitions[(int)State_Code::Forward][(int)Return_Value::Intersection] = State_Code::Branch;
 
     // Branch Transitions
-    transitions[(int)State::Branch][(int)Return_Value::Complete] = State::Detection;
-    transitions[(int)State::Branch][(int)Return_Value::Line_Loss] = State::Backtrack;
-    transitions[(int)State::Branch][(int)Return_Value::Intersection] = State::Forward;
+    transitions[(int)State_Code::Branch][(int)Return_Value::Complete] = State_Code::Detection;
+    transitions[(int)State_Code::Branch][(int)Return_Value::Line_Loss] = State_Code::Backtrack;
+    transitions[(int)State_Code::Branch][(int)Return_Value::Intersection] = State_Code::Forward;
 
     // Backtrack Transitions
-    transitions[(int)State::Backtrack][(int)Return_Value::Complete] = State::Forward;
+    transitions[(int)State_Code::Backtrack][(int)Return_Value::Complete] = State_Code::Forward;
 
     // Gap Transitions
-    transitions[(int)State::Gap][(int)Return_Value::Timeout] = State::Stop;
-    transitions[(int)State::Gap][(int)Return_Value::Complete] = State::Forward;
+    transitions[(int)State_Code::Gap][(int)Return_Value::Timeout] = State_Code::Stop;
+    transitions[(int)State_Code::Gap][(int)Return_Value::Complete] = State_Code::Forward;
 
     // Detection Transitions
-    transitions[(int)State::Detection][(int)Return_Value::Timeout] = State::Backtrack;
+    transitions[(int)State_Code::Detection][(int)Return_Value::Timeout] = State_Code::Backtrack;
 }
+
+
+
+
 
 
 int main() {
-    
+    setup();
 
-    std::cout << "State: " << NAME(State::Forward) << std::endl;
+    Input input = {"This is a nice input"};
+    Output output = {"The old one"};
+
+
+    std::cout << "State: " << NAME(State_Code::Forward) << std::endl;
+    state.do_behavior(input, output);
+    state.guard(input);
+    state.do_behavior(input, output);
 }
+
+
