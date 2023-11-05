3b1d4e3079a93490e5151f22b50e9fb77fe76b4d
aweller1 <drewchristopherweller15@gmail.com>
2023-11-05 13:41:22 -0500

    Basic Speech Recongition working

---
+++ Speech_Recongition/Speech.py
@@ -0,0 +1,10 @@
+import speech_recognition as sr
+sr.__version__
+r = sr.Recognizer()
+print(sr.Microphone.list_microphone_names())
+mic = sr.Microphone(device_index=0)
+with mic as source:
+    r.adjust_for_ambient_noise(source)
+    audio = r.listen(source)
+
+print(r.recognize_google(audio))
